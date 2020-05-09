9d55abf09f41de32e228f1a12509309549c87b5b
A <A@DESKTOP-1HKGSG9>
2020-05-09 21:01:27 +0900

    모임수정

--- src/main/java/us/flower/dayary/domain/MoimTag.java
+++ src/main/java/us/flower/dayary/domain/MoimTag.java
@@ -9,6 +9,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
@@ -24,6 +27,7 @@ public class MoimTag{
 	@ManyToOne
 	@JoinColumn(name = "MOIM_ID")
 	@JsonIgnore
+	@OnDelete(action= OnDeleteAction.NO_ACTION)
 	private Moim moim;
 
 	@ManyToOne
--- src/main/java/us/flower/dayary/repository/moim/MoimTagRepository.java
+++ src/main/java/us/flower/dayary/repository/moim/MoimTagRepository.java
@@ -1,9 +1,13 @@
 package us.flower.dayary.repository.moim;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import us.flower.dayary.domain.Moim;
 import us.flower.dayary.domain.MoimTag;
 
 public interface MoimTagRepository extends JpaRepository<MoimTag,Long>{
-	void deleteByMoim_id(long id);
+	void deleteByMoim(Moim moim);
+	List<MoimTag> findByMoim_id(long id);
 }
