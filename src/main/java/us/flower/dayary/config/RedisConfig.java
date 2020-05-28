6ec08345801bb7b0147e9c9d417aa2a772d0a50d
choiseongjun <csj2702@naver.com>
2020-05-28 17:06:55 +0900

    Redis 주석

--- src/main/java/us/flower/dayary/config/RedisConfig.java
+++ src/main/java/us/flower/dayary/config/RedisConfig.java
@@ -1,64 +1,64 @@
-package us.flower.dayary.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-@Configuration
-public class RedisConfig {
- 
-    @Value("${spring.redis.host}")
-    private String redisHost;
- 
-    @Value("${spring.redis.port}")
-    private int redisPort;
-    @Value("${spring.redis.channel}")
-    private String redisChannel;
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(redisHost);
-        redisStandaloneConfiguration.setPort(redisPort);
-        // 패스워드 있으면 설정
-//        redisStandaloneConfiguration.setPassword(redisPwd);
-        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
-        return lettuceConnectionFactory;
-    }
- 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return redisTemplate;
-    }
-    @Bean
-    public MessageListenerAdapter messageListenerAdapter() {
-        return new MessageListenerAdapter(new RedisMessageSubscriber());
-    }
- 
-    @Bean
-    public RedisMessageListenerContainer redisContainer() {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(redisConnectionFactory());
-        container.addMessageListener(messageListenerAdapter(), topic());
-        return container;
-    }
- 
-    @Bean
-    public ChannelTopic topic() {
-        return new ChannelTopic(redisChannel);
-    }
-
-}
-
+//package us.flower.dayary.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.listener.ChannelTopic;
+//import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+//import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+//@Configuration
+//public class RedisConfig {
+// 
+//    @Value("${spring.redis.host}")
+//    private String redisHost;
+// 
+//    @Value("${spring.redis.port}")
+//    private int redisPort;
+//    @Value("${spring.redis.channel}")
+//    private String redisChannel;
+//    @Bean
+//    public RedisConnectionFactory redisConnectionFactory() {
+//        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+//        redisStandaloneConfiguration.setHostName(redisHost);
+//        redisStandaloneConfiguration.setPort(redisPort);
+//        // 패스워드 있으면 설정
+////        redisStandaloneConfiguration.setPassword(redisPwd);
+//        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+//        return lettuceConnectionFactory;
+//    }
+// 
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//        redisTemplate.setConnectionFactory(redisConnectionFactory());
+//        redisTemplate.setKeySerializer(new StringRedisSerializer());
+//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        return redisTemplate;
+//    }
+//    @Bean
+//    public MessageListenerAdapter messageListenerAdapter() {
+//        return new MessageListenerAdapter(new RedisMessageSubscriber());
+//    }
+// 
+//    @Bean
+//    public RedisMessageListenerContainer redisContainer() {
+//        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+//        container.setConnectionFactory(redisConnectionFactory());
+//        container.addMessageListener(messageListenerAdapter(), topic());
+//        return container;
+//    }
+// 
+//    @Bean
+//    public ChannelTopic topic() {
+//        return new ChannelTopic(redisChannel);
+//    }
+//
+//}
+//
